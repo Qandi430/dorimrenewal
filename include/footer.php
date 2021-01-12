42cf007edec64a47ad77a00950be00a65fa52bb2
Seungjae Lee <dltmdwo430@gmail.com>
2021-01-13 00:21:50 +0900

    create footer

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,10 @@
     <footer>
-        Footer
+        <div class="footerLeft"></div>
+        <div class="footerRight">
+            <p class="copyright">
+                &copy; Copyright 2021 — All rights reserved —STUDIO x JOBS
+            </p>
+        </div>
     </footer>
 </body>
 </html>
